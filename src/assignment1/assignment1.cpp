e612af1863bb8c1a37a94db4479dfdbb3ec8d051
cybzq37 <cybzq37@gmail.com>
2026-01-19 21:23:53 +0800

    assignment 1 completed

--- src/assignment1/assignment1.cpp
+++ src/assignment1/assignment1.cpp
@@ -15,100 +15,57 @@ constexpr double MY_PI = 3.1415926;
 
 Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
 {
-    Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
-
-    Eigen::Matrix4f translate;
-    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
-        -eye_pos[2], 0, 0, 0, 1;
-
-    view = translate * view;
-
+	Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
+	view.col(3) << -eye_pos[0], -eye_pos[1], -eye_pos[2], 1;
     return view;
 }
 
-Eigen::Matrix4f get_model_matrix(float rotation_angle)
+Eigen::Matrix4f get_model_matrix(float rotation_angle) // 旋转矩阵（绕z轴旋转）
 {
-    Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
-
-    // TODO: Implement this function
-    // Create the model matrix for rotating the triangle around the Z axis.
-    // Then return it.
-
-    // Edit begin
-
-    double theta = rotation_angle/180.0 * MY_PI;
-
-    Eigen::Matrix4f mr;
-
-    mr << std::cos(theta),-std::sin(theta),0,0,
-            std::sin(theta),std::cos(theta),0,0,
-            0,0,1,0,
-            0,0,0,1;
-
-    model = mr * model;
-
+	Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
+	float rad = rotation_angle / 180.0 * MY_PI;
+	float sin = std::sin(rad);
+	float cos = std::cos(rad);
+	model.col(0) << cos, sin, 0, 0;
+	model.col(1) << -sin, cos, 0, 0;
     return model;
-
-    // Edit end
 }
 
-Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
-                                      float zNear, float zFar)
+Eigen::Matrix4f get_projection_matrix( // 构建透视矩阵
+    float eye_fov,  // 垂直视野角
+	float aspect_ratio, // 视口宽高比
+	float zNear, // 近平面
+	float zFar // 远平面
+)
 {
-    // Students will implement this function
-
-    Eigen::Matrix4f projection = Eigen::Matrix4f::Identity();
-
-    // TODO: Implement this function
-    // Create the projection matrix for the given parameters.
-    // Then return it.
-
-    // Edit begin
-
     float angel = eye_fov / 180.0 * MY_PI;
-    float t = zNear * std::tan(angel/2);
-    float r = t * aspect_ratio;
-    float l = -r;
-    float b = -t;
+    float r, l, t, b, n, f;  // 右、左、上、下、近、远
+	n = -zNear; // 注意这里的zNear和zFar是正数，代表距离，但在坐标系中是负数
+	f = -zFar;
+	t = abs(n) * tan(angel / 2); // top
+	b = -t; // bottom
+	r = t * aspect_ratio; // right
+	l = -r; // left
 
-    Eigen::Matrix4f MorthoScale(4,4);
-    MorthoScale << 2/(r - l) , 0, 0, 0,
-            0, 2/(t - b) , 0, 0,
-            0, 0, 2/(zFar - zNear), 0,
-            0, 0, 0, 1;
+	Eigen::Matrix4f m_persp_ortho = Eigen::Matrix4f::Identity();
+    m_persp_ortho << n, 0, 0, 0,
+             0, n, 0, 0,
+             0, 0, n + f, -n * f,
+		     0, 0, 1, 0;
 
-    Eigen::Matrix4f MorthoPos(4,4);
-    MorthoPos << 1, 0, 0, -(r + l)/2,
-            0, 1, 0, -(t + b)/2,
-            0, 0, 1, -(zNear + zFar)/2,
-            0, 0, 0, 1;
+	Eigen::Matrix4f m_ortho = Eigen::Matrix4f::Identity();
+    m_ortho << 2 / (r - l), 0, 0, -(r + l) / 2,
+               0, 2 / (t - b), 0, -(t + b) / 2,
+               0, 0, 2 / (n - f), -(n + f) / 2,
+		       0, 0, 0, 1;
 
-
-    Eigen::Matrix4f Mpersp2ortho(4,4);
-
-    Mpersp2ortho << zNear, 0, 0, 0,
-                0, zNear, 0, 0,
-                0, 0, zNear + zFar, -zNear * zFar,
-                0, 0, 1, 0;
-
-    //为了使得三角形是正着显示的，这里需要把透视矩阵乘以下面这样的矩阵
-    //参考：http://games-cn.org/forums/topic/%e4%bd%9c%e4%b8%9a%e4%b8%89%e7%9a%84%e7%89%9b%e5%80%92%e8%bf%87%e6%9d%a5%e4%ba%86/
-    Eigen::Matrix4f Mt(4,4);
-    Mt << 1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, -1, 0,
-        0, 0, 0, 1;
-    Mpersp2ortho = Mpersp2ortho *Mt;
-
-    projection = MorthoScale * MorthoPos * Mpersp2ortho * projection;
-
-    return projection;
+	return m_ortho * m_persp_ortho;
 
     // Edit end
 }
 
 
-// Edit begin
+// 基于罗德里格斯旋转公式
 Eigen::Matrix4f get_rotation(Vector3f axis, float angel)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
@@ -156,20 +113,20 @@ int run_assignment1(int argc, const char** argv)
             return 0;
     }
 
-    rst::Rasterizer r(700, 700);
+    rst::Rasterizer r(700, 700);    // 创建光栅化器对象
 
-    Eigen::Vector3f eye_pos = {0, 0, 5};
+    Eigen::Vector3f eye_pos = {0, 0, 5}; // 相机位置
 
     // Edit begin
-    Eigen::Vector3f rotate_axis = {1,1,0};
+    Eigen::Vector3f rotate_axis = {1,1,0}; // 旋转轴
     // Edit end
 
-    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}};
+    std::vector<Eigen::Vector3f> pos{{2, 0, -2}, {0, 2, -2}, {-2, 0, -2}}; // 三角形顶点
 
-    std::vector<Eigen::Vector3i> ind{{0, 1, 2}};
+    std::vector<Eigen::Vector3i> ind{{0, 1, 2}}; // 三角形索引
 
-    auto pos_id = r.load_positions(pos);
-    auto ind_id = r.load_indices(ind);
+    auto pos_id = r.load_positions(pos); // 加载顶点位置
+    auto ind_id = r.load_indices(ind); // 加载顶点索引
 
     int key = 0;
     int frame_count = 0;
